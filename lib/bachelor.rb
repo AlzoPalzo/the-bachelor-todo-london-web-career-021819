8421aa7ebe2d5b36efd1f5130bce642c85111085
Ali Madni <ali555m@hotmail.com>
2019-02-13 12:49:50 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data.each do |s, contestant|
     if s == season
-      contestant.each do |contestant_data, val|
+      contestant.each do |contestant_data|
         if contestant_data["status"] == "Winner"
           name_ary = contestant_data["name"].split(" ")
           return name_ary[0]
